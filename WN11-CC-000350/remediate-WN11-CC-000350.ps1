512df3a66818aad5b318009c25eae13d6b27616b
Nehal Patel <nehalcanadaedu@gmail.com>
2026-06-05 23:31:50 -0400

    Update remediate-WN11-CC-000350.ps1

--- WN11-CC-000350/remediate-WN11-CC-000350.ps1
+++ WN11-CC-000350/remediate-WN11-CC-000350.ps1
@@ -1,30 +1,3 @@
-<#
-.SYNOPSIS
-    This PowerShell script prevents the WinRM service from using unencrypted traffic via Group Policy Registry settings.
-
-.NOTES
-    Author          : Aniket Agarwal
-    LinkedIn        : linkedin.com/in/aniket-agarwal-0920
-    GitHub          : github.com/Aaniket09
-    Date Created    : 2025-11-26
-    Last Modified   : 2025-11-26
-    Version         : 1.0
-    CVEs            : N/A
-    Plugin IDs      : N/A
-    STIG-ID         : WN11-CC-000350
-
-.TESTED ON
-    Date(s) Tested  : 2025-02-26
-    Tested By       : Aniket Agarwal
-    Systems Tested  : Windows 11 Pro (24H2)
-    PowerShell Ver. : 5.1, 7.4
-
-.USAGE
-    Run this script as Administrator.
-    Example syntax:
-    PS C:\> .\remediate_WN11-CC-000350.ps1
-#>
-
 # Define the Registry path and value details
 # We use the Policy path to enforce this setting strictly
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service"
